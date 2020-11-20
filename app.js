a071895953d409aa2b91a0ee8c2844531c13d83d
unknown <takadzejaba@gmail.com>
2020-11-20 14:19:42 +0300

    davaleba

---
+++ app.js
@@ -0,0 +1,10 @@
+var cursor = document.getElementById('cursor');
+
+document.addEventListener('mousemove'
+, function(e){
+    var x = e.clientX;
+    var y = e.clientY;
+    cursor.style.left = x + "px";
+    cursor.style.top = y + "px";
+    console.log(y);
+})
